913a5f057a6dee0ac2b8b153c1679042c840aca9
Pranjali Ghule <166767155+pranjalighule05@users.noreply.github.com>
2026-01-08 19:18:56 +0530

    Implement Doubly Linked List

--- Singly Linked List/src/Doubly_linked_list.java
+++ Singly Linked List/src/Doubly_linked_list.java
@@ -1,4 +1,54 @@
-
 public class Doubly_linked_list {
 
+    // Node class
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+
+    // Insert at end
+    void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+    }
+
+    // Display list
+    void display() {
+        Node temp = head;
+        System.out.print("Doubly Linked List: ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Doubly_linked_list list = new Doubly_linked_list();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        list.display();
+    }
 }
